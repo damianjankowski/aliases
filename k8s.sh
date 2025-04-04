79af1cf0e1453b9871bdaea78fd2cd268b9048ea
Damian Jankowski <djankowski.dev@gmail.com>
2025-04-04 18:40:24 +0200

    feat: add k8s aliases

---
+++ k8s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ---- Kubernetes aliases ----
+alias k="kubectl"
+alias kgp="kubectl get pods"
+alias kgs="kubectl get services"
+alias kgd="kubectl get deployments"
+alias kgn="kubectl get nodes"
+alias kga="kubectl get all"
+alias kd="kubectl describe"
+alias kl="kubectl logs"
+alias kex="kubectl exec -it"
+alias kns="kubectl config set-context --current --namespace"
+alias krestart="kubectl rollout restart deployment"
+alias kdelpods="kubectl delete pod"
+alias klog='kubectl logs -f'        
