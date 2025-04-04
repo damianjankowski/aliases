e5299e8654bd8dd4f5f93a21fd31d2bea97e6777
Damian Jankowski <djankowski.dev@gmail.com>
2025-04-04 18:40:08 +0200

    feat: add homebrew aliases

---
+++ homebrew.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# ---- Homebrew aliases ----
+alias brewup="brew update && brew outdated"
+alias brewupg="brew upgrade && brew cleanup"
+alias fullbrew="brew update && brew upgrade && brew cleanup && brew doctor"
