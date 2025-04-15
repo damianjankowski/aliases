34ab5e14f4b96f55d45c1ba11f47c438b413542b
Damian Jankowski <djankowski.dev@gmail.com>
2025-04-15 08:12:43 +0200

    feat: add command for brew uninstall to remove all files associated with a cask

--- homebrew.sh
+++ homebrew.sh
@@ -3,3 +3,4 @@
 alias brewup="brew update && brew outdated"
 alias brewupg="brew upgrade && brew cleanup"
 alias fullbrew="brew update && brew upgrade && brew cleanup && brew doctor"
+alias brewuninstall="brew uninstall --zip"
